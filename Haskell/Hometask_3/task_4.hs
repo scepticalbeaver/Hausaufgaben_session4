081f0156fdebe00ee41cd96770e5a3619868c4e0
Ivan Senin <senin-i-i@yandex.ru>
2014-03-11 22:39:01 +0400

    Task4 fix

--- Haskell/Hometask_3/task_4.hs
+++ Haskell/Hometask_3/task_4.hs
@@ -4,7 +4,7 @@ findMaxNeighbours :: (Enum a, Num a, Ord a) => [a] -> a
 findMaxNeighbours [] = 0
 findMaxNeighbours [x] = 1
 findMaxNeighbours (x : xs) =
-    snd (maximumBy maxFst (zip (zipWith(+) (0 : x : xs) (xs ++ [0] ++ [0])) [1..])) where
+    snd (maximumBy maxFst (zip (zipWith(+) (x : xs) (xs ++ [0])) [1..])) where
 
         maxFst :: (Ord a) => (a, b) -> (a, b) -> Ordering
         maxFst a b 
@@ -14,6 +14,8 @@ findMaxNeighbours (x : xs) =
 
 test =
     findMaxNeighbours [1, 5, 6, 2] == 2
-    && findMaxNeighbours [1,7,9,11,17,19,71,77,79,91] == 9
+    && findMaxNeighbours [1, 7, 9, 11, 17, 19, 71, 77, 79, 91] == 9
     && findMaxNeighbours [0, 9, 0] == 1
-    && findMaxNeighbours [1, 2, 3, 12, 0, 12, 0, 12, 3, 2, 1] == 5
+    && findMaxNeighbours [1, 2, 3, 12, 0, 12, 0, 12, 3, 2, 1] == 3
+    && findMaxNeighbours [1, 5, 7, 4, 13, 4] == 4
+    && findMaxNeighbours [1, 5, 7, 4, 213, 31] == 5
