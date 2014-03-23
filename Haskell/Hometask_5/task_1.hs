cefd9256a359cdbc8406804a67519b134b50d4bf
Ivan Senin <senin-i-i@yandex.ru>
2014-03-23 19:58:41 +0400

    H5_Task1 done

--- Haskell/Hometask_5/task_1.hs
+++ Haskell/Hometask_5/task_1.hs
@@ -1,18 +1,7 @@
---Реализовать функцию, которая по заданному числу n выводит все его разложения на положительные слагаемые (с точностью до порядка разложения)
--- Task: decompose integer to sum
 module Task1 where
 
 import Data.List
 import Control.Monad
---import Data.Set (toList, fromList)
-
---rmDublicates = toList . fromList
-
--- some exp algo
---decomp n = sort (helper n n)
---    where 
---    helper n 0 = [[]]
---    helper n len = rmDublicates (map (\ xs -> sort xs) (filter  (\ xs -> (sum xs) == n) (replicateM len [1..n]))) ++ (helper n (len - 1))
 
 doDecompose :: Int -> Int -> [Int] -> IO()
 doDecompose 0 _ xs = putStrLn (show xs)
