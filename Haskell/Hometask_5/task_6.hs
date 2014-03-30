4f877095e59b6124a87c8503649c5f27066f116e
Ivan Senin <senin-i-i@yandex.ru>
2014-03-31 00:38:50 +0400

    H5Task_6 done. Simplifying within

--- Haskell/Hometask_5/task_6.hs
+++ Haskell/Hometask_5/task_6.hs
@@ -17,8 +17,8 @@ infixl 8 `Power`
 instance Show Expression
     where
     show (Plus a b) = "(" ++ show a ++ " + " ++ show b ++ ")"
-    show (Product a b) = "(" ++ show a ++ " * " ++ show b ++ ")"
-    show (Power a d) = "(" ++ show a ++ "^(" ++ show d ++ "))"
+    show (Product a b) = show a ++ " * " ++ show b
+    show (Power a d) = show a ++ "^(" ++ show d ++ ")"
     show Variable = "x"
     show (Const x) = show x
 
@@ -34,6 +34,7 @@ simplify' Variable _ = Variable
 simplify' (Const x) _ = Const x
 simplify' (Product x y) guard = case (simplify' x True, simplify' y True) of
                                 (Const a, Const b) -> Const (a * b)
+                                (Variable, Variable) -> Power Variable 2
                                 (Const 0, _) -> Const (0)
                                 (_, Const 0) -> Const (0)
                                 (Const 1, z) -> simplify' z True
@@ -41,38 +42,71 @@ simplify' (Product x y) guard = case (simplify' x True, simplify' y True) of
                                 (Variable, Power Variable d) -> Variable `Power` (d + 1)
                                 (Power Variable d, Variable) -> Variable `Power` (d + 1)
                                 (z, Const k) -> simplify' ((Const k) `Product` (simplify' z True)) True
-                                (Const k, z) -> Const k `Product` (simplify' z True)
-                                --(Const k1, ((Const k2) `Product` z)) -> (Const k1 * k2) `Product` (simplify' z True)
+                                (Const k1, z) -> siftDownProduction (Const k1) (simplify' z True)
+                                (Variable, z) -> siftDownProduction (Variable) (simplify' z True)
                                 (l, r) -> if guard 
                                           then Product (simplify' l False) (simplify' r False)
                                           else Product l r
+
 simplify' (Plus x y) guard = case (simplify' x True, simplify' y True) of
                             (Const a, Const b) -> Const (a + b)
                             (Const 0, z) -> simplify' z True
                             (z, Const 0) -> simplify' z True
                             (Variable, Variable) -> Const 2 `Product` Variable
                             (Const a, Variable) -> Variable `Plus` Const a
+                            (Const k, z) -> siftDownAddition (Const k) (simplify' z True)
+                            (z, Const k) -> siftDownAddition (Const k) (simplify' z True) 
                             (l, r) -> if guard
                                       then Plus (simplify' l False) (simplify' r False)
                                       else Plus l r
 simplify' (Power Variable 0) _ = Const 1
 simplify' z _ = z
 
---siftDown :: Expression -> Expression -> Maybe Expression
---siftDown (Count k) sub = case sub of
---                         (Const k2, z) -> Just ((Const (k * k2)), z) 
---                         otherwise -> Nothing
+siftDownProduction :: Expression -> Expression -> Expression
+siftDownProduction coeff sub = case sub of
+                        (Variable) -> Product coeff Variable
+                        (Product x y) ->  Product (simplify $ siftDownProduction coeff x)  y
+                        (Plus x y) -> Plus (simplify $ siftDownProduction coeff x) (simplify $ siftDownProduction coeff y)
+                        other -> Product coeff other
+
+siftDownAddition :: Expression -> Expression -> Expression
+siftDownAddition coeff sub = case (coeff, sub) of
+                        (coeff, Variable) -> Plus coeff Variable
+                        (Const k, Plus x y) -> Plus x (simplify $ siftDownAddition (Const k) y)
+                        (z, Plus x y) -> Plus (simplify $ siftDownAddition coeff x) y
+                        (Const k, z) -> Plus z coeff
+                        (coeff, other) -> Plus coeff other
 
 simplify :: Expression -> Expression
 simplify z = simplify' z True
 
-simpleExpr = (Const 5) `Plus` Variable `Product` Variable  -- "5 + x * x"
-
-
-polynomOfFour = (Power Variable 3) `Plus` Variable `Product` (Power Variable (-1)) `Plus` (Const 2) `Product` (Power Variable 4)
-polyNON = (Const 2) `Product` (Const 3) `Plus` (Const 4)
-
 
 make = simplify . derivate . simplify
 
-test1 = make simpleExpr  --(2 * x)
+------------ Test cases
+
+-- Polynom examples
+
+-- "5 + x * x"
+simpleExpr = (Const 5) `Plus` Variable `Product` Variable
+
+-- x^3 + x * x^(-1) + 2 * x^4
+polynomOfFour = (Power Variable 3) `Plus` Variable `Product` (Power Variable (-1)) `Plus` (Const 2) `Product` (Power Variable 4)
+
+-- 2 * 3 + 4
+justExpr = (Const 2) `Product` (Const 3) `Plus` (Const 4)
+
+-- x * (x + 3)
+treeCheckProduct = (Variable) `Product` (Variable `Plus` Const 3)
+
+-- 3 + (x + 4)
+treeCheckPlus = (Const 3) `Plus` (Variable `Plus` Const 4)
+
+test0 = make simpleExpr  --(2 * x)
+
+test_s1 = simplify simpleExpr       -- x^2 + 5
+test_s2 = simplify polynomOfFour    -- x^3 + 1 + 2 * x^4
+test_s3 = simplify treeCheckProduct -- x^2 + 3*x
+test_s4 = simplify treeCheckPlus    -- x + 7
+
+test = make polynomOfFour --8 * x^3 + 3 * x^2
